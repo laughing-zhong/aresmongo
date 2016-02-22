21c9858b4c1a955118d52f28e494ace206664856
laughing-zhong <wiqi.zhong@gmail.com>
2016-02-22 19:11:35 +0800

    change name

--- mygameserver/ares-app/src/main/java/com/ares/app/admincontroller/AdminService.java
+++ mygameserver/ares-app/src/main/java/com/ares/app/admincontroller/AdminService.java
@@ -4,25 +4,25 @@ package com.ares.app.admincontroller;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
+
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.ares.app.domain.Do.AdminDO;
+
 
 
 @Controller
-public class AdminService   {
-	
-	
+public class AdminService   {	
 	@RequestMapping(value = "/admin",method=RequestMethod.GET)
 	public  String  getAdminList(Model model){
 		
-		List<AdminPlayer> playerList = new ArrayList<AdminPlayer>();
+		List<AdminDO> playerList = new ArrayList<AdminDO>();
 		for( int i = 0 ; i < 5; ++i){
-			AdminPlayer player = new AdminPlayer();
+			AdminDO player = new AdminDO();
 			player.setId(i+"");
 			player.setName("name"+i);
 			playerList.add(player);
@@ -32,11 +32,11 @@ public class AdminService   {
 	}
 	
 	@RequestMapping(value = "/admin/save/admin",method=RequestMethod.POST)
-	public  String  saveAdmin(AdminPlayer adminPlayer,Model model){
+	public  String  saveAdmin(AdminDO AdminDO,Model model){
 		
-		List<AdminPlayer> playerList = new ArrayList<AdminPlayer>();
+		List<AdminDO> playerList = new ArrayList<AdminDO>();
 		for( int i = 0 ; i < 5; ++i){
-			AdminPlayer player = new AdminPlayer();
+			AdminDO player = new AdminDO();
 			player.setId(i+"");
 			player.setName("name"+i);
 			playerList.add(player);
@@ -45,46 +45,4 @@ public class AdminService   {
 		return "/admin/adduser";	
 	}
 	
-	
-	
-	
-	public static class AdminPlayer{
-		private String id;
-		private String  email;
-		private String passwd;
-		public String getPasswd() {
-			return passwd;
-		}
-		public void setPasswd(String passwd) {
-			this.passwd = passwd;
-		}
-		public String getEmail() {
-			return email;
-		}
-		public void setEmail(String email) {
-			this.email = email;
-		}
-		public String getTeleno() {
-			return teleno;
-		}
-		public void setTeleno(String teleno) {
-			this.teleno = teleno;
-		}
-		private String teleno;
-		public String getId() {
-			return id;
-		}
-		public void setId(String id) {
-			this.id = id;
-		}
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		private String name;
-	}
-	
-
 }
