443f169d4d79c30c3fbd2fc19e6fa9f6951df784
laughing-zhong <wiqi.zhong@gmail.com>
2016-03-13 22:49:30 +0800

    for user login

--- mygameserver/ares-app/src/main/java/com/ares/app/service/UserLoginService.java
+++ mygameserver/ares-app/src/main/java/com/ares/app/service/UserLoginService.java
@@ -6,22 +6,26 @@ import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 
 import com.ares.app.bean.AccountBean;
-import com.ares.app.dao.EeUserDAO;
-import com.ares.app.domain.Do.EeUserDO;
+import com.ares.app.dao.AccountDAO;
+import com.ares.app.domain.Do.AccountDO;
 import com.ares.framework.service.IService;
 
 @Component
 public class UserLoginService  implements IService{
-	
-	@Inject 
-	private EeUserDAO userDAO;
+	@Inject
+	private AccountDAO accountDAO;
 	public String  login(AccountBean account,Model model){
-		EeUserDO userDO = userDAO.findById(account.getAccountID());
+		AccountDO userDO = accountDAO.findById(account.getAccountID());
 		if(userDO == null){
 			model.addAttribute("errormsg", "login faild");
 			return "404";
 		}
-		return "defalut";		
+		if ( userDO.getPassword().equals(account.getPwd()) )
+		   return "default";	
+		else {
+			model.addAttribute("errormsg", "id and password not match");
+			return "404";
+		}
 	}
 
 }
