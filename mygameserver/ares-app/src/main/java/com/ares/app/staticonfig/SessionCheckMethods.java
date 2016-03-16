9f69dbd550dbee69052925d62cc057205006b0ca
laughing-zhong <wiqi.zhong@gmail.com>
2016-03-16 19:09:14 +0800

    update

---
+++ mygameserver/ares-app/src/main/java/com/ares/app/staticonfig/SessionCheckMethods.java
@@ -0,0 +1,20 @@
+package com.ares.app.staticonfig;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+import java.util.Set;
+
+public class SessionCheckMethods {
+	private   Properties addressProp;
+	
+	private Set<String> checkMethodsMap = new HashSet<String>();
+	public SessionCheckMethods(Map<String, String>checkedMethods){
+		for(Entry<String, String> entity : checkedMethods.entrySet()){
+			//addressProp.g
+			
+		}
+	}
+
+}
